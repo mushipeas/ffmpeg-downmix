977ba851fedc879f60ccddadfe529e52e11c5871
mushipeas <8151412+mushipeas@users.noreply.github.com>
2021-10-04 23:09:32 +0100

    clean up formatting

--- downmix.sh
+++ downmix.sh
@@ -14,8 +14,7 @@ cd /workdir
 
 INPUT_FILENAME="$1"
 ## Set output_filename to arg#2 if given, else to {input_filename}_new.
-if [ -z "$2" ]
-then
+if [ -z "$2" ]; then
     OUTPUT_FILENAME=$(addext $INPUT_FILENAME "new")
 else
     OUTPUT_FILENAME="$2"
@@ -24,31 +23,26 @@ fi
 AUDIO_CHANNELS=$(ffprobe -v error -select_streams a:0 -show_entries stream=channel_layout -of default=noprint_wrappers=1:nokey=1 "$1")
 #AUDIO_CHANNELS=$(ffprobe -v error -show_streams -select_streams a "$1" | grep -Po "(?<=^channel_layout\=)\d*\.\d*")
 
-if [ "$AUDIO_CHANNELS" == '5.1' ]
-then
+if [ "$AUDIO_CHANNELS" == '5.1' ]; then
     # 5.1 Surround
     echo "5.1 Surround detected."
     FL="0.5*FC+0.707*FL+0.707*BL+0.5*LFE"
     FR="0.5*FC+0.707*FR+0.707*BR+0.5*LFE"
-elif [ "$AUDIO_CHANNELS" == '5.1(side)' ]
-then
+elif [ "$AUDIO_CHANNELS" == '5.1(side)' ]; then
     # 5.1(side) Surround
     echo "5.1(side) Surround detected."
     FL="0.5*FC+0.707*FL+0.707*SL+0.5*LFE"
     FR="0.5*FC+0.707*FR+0.707*SR+0.5*LFE"
-elif [ "$AUDIO_CHANNELS" == '6.1' ]
-then
+elif [ "$AUDIO_CHANNELS" == '6.1' ]; then
     # 6.1 Surround
     echo "6.1 Surround detected."
     FL="0.321953*FC+0.455310*FL+0.394310*SL+0.227655*SR+0.278819*BC+0.321953*LFE"
     FR="0.321953*FC+0.455310*FR+0.394310*SR+0.227655*SL+0.278819*BC+0.321953*LFE"
-elif [ "$AUDIO_CHANNELS" == '7.1' ]
-then
+elif [ "$AUDIO_CHANNELS" == '7.1' ]; then
     # 7.1 Surround
     echo "7.1 Surround detected."
     FL="0.274804*FC+0.388631*FL+0.336565*SL+0.194316*SR+0.336565*BL+0.194316*BR+0.274804*LFE"
     FR="0.274804*FC+0.388631*FR+0.336565*SR+0.194316*SL+0.336565*BR+0.194316*BL+0.274804*LFE"
-
 else
     echo "$INPUT_FILENAME: No primary surround sound source detected. Stereo sound primary may already be present."
     exit 0
@@ -57,8 +51,7 @@ echo "$INPUT_FILENAME: Using FL:$FL | FR:$FR"
 
 ## Audio bitrate is set the same as the incoming video. Fallback to 192k.
 AUDIO_BRT=$(ffprobe -v error -select_streams a:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 "$1")
-if [ -z "$AUDIO_BRT" ]
-then
+if [ -z "$AUDIO_BRT" ]; then
     echo "Default bitrate: 192k"
     AUDIO_BRT="192k"
 fi
@@ -84,8 +77,7 @@ ffmpeg -i "$INPUT_FILENAME" -vn \
 
 DOWNMIX_AUDIO_STATUS=$?
 
-if [ $DOWNMIX_AUDIO_STATUS -eq 0 ]
-then
+if [ $DOWNMIX_AUDIO_STATUS -eq 0 ]; then
     ffmpeg -i "$INPUT_FILENAME" -i .temp_stereo_audio.m4a -map 0:v:0 \
         -map 1:a -map 0:a \
         -map 0:s -c copy \
