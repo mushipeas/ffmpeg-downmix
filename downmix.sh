12c4fad063e60d591fa4464fb7226240974bbf78
mushipeas <8151412+mushipeas@users.noreply.github.com>
2021-10-17 09:04:02 +0100

    replace if chain with case

--- downmix.sh
+++ downmix.sh
@@ -24,30 +24,36 @@ TEMP_STEREO_AUDIO=".temp_stereo_audio_$RANDOM-$RANDOM-$RANDOM.m4a"
 AUDIO_CHANNELS=$(ffprobe -v error -select_streams a:0 -show_entries stream=channel_layout -of default=noprint_wrappers=1:nokey=1 "$1")
 #AUDIO_CHANNELS=$(ffprobe -v error -show_streams -select_streams a "$1" | grep -Po "(?<=^channel_layout\=)\d*\.\d*")
 
-if [ "$AUDIO_CHANNELS" == '5.1' ]; then
-    # 5.1 Surround
-    echo "5.1 Surround detected."
-    FL="0.5*FC+0.707*FL+0.707*BL+0.5*LFE"
-    FR="0.5*FC+0.707*FR+0.707*BR+0.5*LFE"
-elif [ "$AUDIO_CHANNELS" == '5.1(side)' ]; then
-    # 5.1(side) Surround
-    echo "5.1(side) Surround detected."
-    FL="0.5*FC+0.707*FL+0.707*SL+0.5*LFE"
-    FR="0.5*FC+0.707*FR+0.707*SR+0.5*LFE"
-elif [ "$AUDIO_CHANNELS" == '6.1' ]; then
-    # 6.1 Surround
-    echo "6.1 Surround detected."
-    FL="0.321953*FC+0.455310*FL+0.394310*SL+0.227655*SR+0.278819*BC+0.321953*LFE"
-    FR="0.321953*FC+0.455310*FR+0.394310*SR+0.227655*SL+0.278819*BC+0.321953*LFE"
-elif [ "$AUDIO_CHANNELS" == '7.1' ]; then
-    # 7.1 Surround
-    echo "7.1 Surround detected."
-    FL="0.274804*FC+0.388631*FL+0.336565*SL+0.194316*SR+0.336565*BL+0.194316*BR+0.274804*LFE"
-    FR="0.274804*FC+0.388631*FR+0.336565*SR+0.194316*SL+0.336565*BR+0.194316*BL+0.274804*LFE"
-else
-    echo "$INPUT_FILENAME: No primary surround sound source detected. Stereo sound primary may already be present."
-    exit 0
-fi
+case $AUDIO_CHANNELS in
+    '5.1')
+        # 5.1 Surround
+        echo "5.1 Surround detected."
+        FL="0.5*FC+0.707*FL+0.707*BL+0.5*LFE"
+        FR="0.5*FC+0.707*FR+0.707*BR+0.5*LFE"
+        ;;
+    '5.1(side)')
+        # 5.1(side) Surround
+        echo "5.1(side) Surround detected."
+        FL="0.5*FC+0.707*FL+0.707*SL+0.5*LFE"
+        FR="0.5*FC+0.707*FR+0.707*SR+0.5*LFE"
+        ;;
+    '6.1')
+        # 6.1 Surround
+        echo "6.1 Surround detected."
+        FL="0.321953*FC+0.455310*FL+0.394310*SL+0.227655*SR+0.278819*BC+0.321953*LFE"
+        FR="0.321953*FC+0.455310*FR+0.394310*SR+0.227655*SL+0.278819*BC+0.321953*LFE"
+        ;;
+    '7.1')
+        # 7.1 Surround
+        echo "7.1 Surround detected."
+        FL="0.274804*FC+0.388631*FL+0.336565*SL+0.194316*SR+0.336565*BL+0.194316*BR+0.274804*LFE"
+        FR="0.274804*FC+0.388631*FR+0.336565*SR+0.194316*SL+0.336565*BR+0.194316*BL+0.274804*LFE"
+        ;;
+    '5.1')
+        echo "$INPUT_FILENAME: No primary surround sound source detected. Stereo sound primary may already be present."
+        exit 0
+        ;;
+esac
 echo "$INPUT_FILENAME: Using FL:$FL | FR:$FR"
 
 ## Audio bitrate is set the same as the incoming video. Fallback to 192k.
